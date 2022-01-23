87f978f6af2cae75ec95fe0bf015e9a92bb2a4f9
Jussi Kupari <jkupari@gmail.com>
2022-01-23 19:14:42 +0100

    some progress

--- Part4/Ch23/Ex2364.rkt
+++ Part4/Ch23/Ex2364.rkt
@@ -20,25 +20,25 @@ Hint: This will probably require more than one helper function. Take it one step
 a time: try to write the main function, figure out what you need to do to the recursive
 result, invent a function to do that, and repeat until what you need to do is built-in.
 
-I used naturals here. This was definitely quite a challenge and I ended up doing quite a
+I used Atoms here. This was definitely quite a challenge and I ended up doing quite a
 lot of trial-and-error work. the list-of-lists structure makes me trip easily - especially
 the 'empty' - when using the different annotation types ('list' and 'cons').
 |#
 
 ;; -- Data Definitions
 
-;; List-of-Numbers (LON) is one of:
+;; List-of-Atoms (LOA) is one of:
 ;; - empty
-;; - (cons Number LON)
+;; - (cons Atom LOA)
 
-;; List-of-LON (LOLON) is one of:
+;; List-of-LOA (LOLOA) is one of:
 ;; - empty
-;; - (cons LON empty)
+;; - (cons LOA empty)
 
 ;; -- Functions
 
-;; scramble :: LON -> LOLON
-;; Given a LON, produces a LOLON of all permutations the elements in LON
+;; scramble :: LOA -> LOLOA
+;; Given a LOA, produces a LOLOA of all permutations the elements in LOA
 (check-expect (scramble empty) (list empty))
 (check-expect (scramble (list 1 2 3))
               (list
@@ -49,26 +49,26 @@ the 'empty' - when using the different annotation types ('list' and 'cons').
                (list 2 1 3)
                (list 1 2 3)))
 
-(define (scramble lon)
+(define (scramble loa)
   (cond
-    [(empty? lon) (list empty)]
-    [else (insert-in-all-lists (first lon) (scramble (rest lon)))]))
+    [(empty? loa) (list empty)]
+    [else (insert-in-all-lists (first loa) (scramble (rest loa)))]))
 
-;; insert-in-all-lists : Natural LOLON -> LOLON
-;; Given natural and LOLON, produces a LOLON where natural is inserted in all position in lists
+;; insert-in-all-lists : Atom LOLOA -> LOLOA
+;; Given Atom and LOLOA, produces a LOLOA where Atom is inserted in all position in lists
 (check-expect (insert-in-all-lists 1 empty) empty)
 (check-expect (insert-in-all-lists 1 (list (list 2 3) (list 3 2)))
               (list
                (list 2 3 1) (list 2 1 3) (list 1 2 3) (list 3 2 1) (list 3 1 2) (list 1 3 2)))
 
-(define (insert-in-all-lists n lolon)
+(define (insert-in-all-lists a loloa)
   (cond
-    [(empty? lolon) empty]
-    [else (append (insert-at-all-positions n (first lolon))
-                  (insert-in-all-lists n (rest lolon)))]))
+    [(empty? loloa) empty]
+    [else (append (insert-at-all-positions a (first loloa))
+                  (insert-in-all-lists a (rest loloa)))]))
 
-;; insert-at-all-positions : Natural LON -> LOLON
-;; Given natural and a LON, produces a LOLON where natural is inserted in all possible positions
+;; insert-at-all-positions : Atom LOA -> LOLOA
+;; Given Atom and a LOA, produces a LOLOA where Atom is inserted in all possible positions
 (check-expect (insert-at-all-positions 1 empty)
               (list (list 1)))
 (check-expect (insert-at-all-positions 1 (list 2 3))
@@ -77,12 +77,12 @@ the 'empty' - when using the different annotation types ('list' and 'cons').
                (list 2 1 3)
                (list 1 2 3)))
 
-(define (insert-at-all-positions n lon)
-  (insert-at-positions (length lon) n lon))
+(define (insert-at-all-positions a loa)
+  (insert-at-positions (length loa) a loa))
 
-;; insert-at-positions : Index Natural LON -> LOLON
-;; Given natural and LON
-;; produces a LOLON where the natural is inserted at each position starting from zero to index
+;; insert-at-positions : Index Atom LOA -> LOLOA
+;; Given Atom and LOA
+;; produces a LOLOA where the Atom is inserted at each position starting from zero to Index
 (check-expect (insert-at-positions 0 1 empty)
               (list (list 1)))
 (check-expect (insert-at-positions 0 1 (list 2 3))
@@ -93,22 +93,22 @@ the 'empty' - when using the different annotation types ('list' and 'cons').
                (list 2 1 3)
                (list 1 2 3)))
 
-(define (insert-at-positions i n lon)
+(define (insert-at-positions i a loa)
   (cond
-    [(empty? lon) (list (list n))]
-    [(zero? i) (list (cons n lon))]
-    [else (cons (insert-at-position i n lon)
-                (insert-at-positions (sub1 i) n lon))]))
+    [(empty? loa) (list (list a))]
+    [(zero? i) (list (cons a loa))]
+    [else (cons (insert-at-position i a loa)
+                (insert-at-positions (sub1 i) a loa))]))
 
-;; insert-at-position : Index Natural LON -> LON
-;; Given index, natural and LON, inserts the natural in the list at given index
+;; insert-at-position : Index Atom LOA -> LOA
+;; Given Index, Atom and LOA, inserts the Atom in the list at given Index
 (check-expect (insert-at-position 1 1 empty) (list 1))
 (check-expect (insert-at-position 0 1 (list 2 3)) (list 1 2 3))
 (check-expect (insert-at-position 1 1 (list 2 3)) (list 2 1 3))
 (check-expect (insert-at-position 2 1 (list 2 3)) (list 2 3 1))
 
-(define (insert-at-position i n lon)
+(define (insert-at-position i a loa)
   (cond
-    [(empty? lon) (list n)]
-    [(zero? i) (append (list n) lon)]
-    [else (cons (first lon) (insert-at-position (sub1 i) n (rest lon)))]))
+    [(empty? loa) (list a)]
+    [(zero? i) (append (list a) loa)]
+    [else (cons (first loa) (insert-at-position (sub1 i) a (rest loa)))]))
