0fdf6bad935cbf3e0b6121997515a37e51ea7833
Jussi Kupari <jkupari@gmail.com>
2022-01-24 18:28:02 +0100

    progress in Ch25

--- Part4/Ch25/Ex25415.rkt
+++ Part4/Ch25/Ex25415.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname Ex25415_WOP) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname Ex25415) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|Exercise 25.4.15
 Develop a function lcsubsequence (“longest common subsequence”) that takes in
 two strings and returns the longest string which is a subsequence of both of
@@ -16,6 +16,10 @@ Hint: As in exercise 25.4.14, your program may be slow. My first attempt took ab
 know of a more efficient way to do it using what you’ve already seen. A technique called
 dynamic programming or memoization, which we’ll discuss in Chapter 30, enabled me to
 do it in about 0.01 seconds.Again, the difference is more dramatic for longer strings.
+
+My solution seems quite inefficient:
+(time (lcsubsequence "mickey mouse" "minnie moose"))
+cpu time: 6875 real time: 6977 gc time: 156
 |#
 
 ;; -- Data Definitions
