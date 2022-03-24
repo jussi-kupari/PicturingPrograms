4be2e646887a29a7261f3461b5a709a52c485fb5
Jussi Kupari <jkupari@KI-CND1457CS2.user.ki.se>
2022-03-24 17:46:10 +0100

    small progress

--- Part5/Ch27/Ex2743.rkt
+++ Part5/Ch27/Ex2743.rkt
@@ -1,11 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-reader.ss" "lang")((modname Ex2743_wip) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname Ex2743) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|Exercise 27.4.3
 Develop a function substitute-color that takes in two colors and
 an image, and replaces every pixel which is the first color with the second color.
 
-Note: At first I had col1 variable both the outer and inner functions and that caused the
+Note: At first I had a col1 variable in both the outer and inner functions and that caused the
 (color=? col1 (get-pixel-color x y img)) to evaluate to
 (color=? (get-pixel-color x y img) (get-pixel-color x y img))
 
